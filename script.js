9dce0578f1c3810ddee4dfacd2072482b0d68d05
ArtemProkofev <artem_artem00@rambler.ru>
2016-10-18 21:31:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,13 +39,25 @@
         	c3_elem.style.display = 'block';
      }
         function check() {
+	    var result = 0;
             q1elem = document.getElementById("q1");
             q2elem = document.getElementById("q2");
             q3elem = document.getElementById("q3");
             q4elem = document.getElementById("q4");
             q5elem = document.getElementById("q5");
             q6elem = document.getElementById("q6");
+	    rr1elem = document.getElementById("rr1");
+            rr2elem = document.getElementById("rr2");
+            rr3elem = document.getElementById("rr3");
+            rr4elem = document.getElementById("rr4");
             t1elem = document.getElementById("t1");
-            answer = t1elem.value;
-            alert(answer);
+            if (q1elem.checked == false) result = result + 1;
+	    if (q2elem.checked == false) result = result + 1;
+	    if (q3elem.checked == true) result = result + 1;
+	    if (q4elem.checked == true) result = result + 1;
+	    if (q5elem.checked == false) result = result + 1;
+            if (q6elem.checked == true) result = result + 1;
+            if (rr1elem.checked == true) result = result + 1;
+            if (t1elem.value == 'Sealand') result = result + 1; 
+            alert(result);
         }
